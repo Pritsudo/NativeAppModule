f3d142b2103db95f5621911c8a2237acd85cd2de
Prit <20dit008@charusat.edu.in>
2022-06-30 13:11:33 +0530

    storing and fetching data using sql

--- lib/provider/great_places.dart
+++ lib/provider/great_places.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 
 import '../model/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -20,5 +21,23 @@ class GreatPlaces with ChangeNotifier {
             PlaceLocation(latitude: 2.333, longitude: 5.222, address: ''));
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
   }
-}
+
+    Future<void> fetchAndSetPlaces() async {
+      final dataList = await DBHelper.getData('user_places');
+      _items = dataList
+          .map((item) => Place(
+              id: item['id'],
+              title: item['title'],
+              image: File(item['image']),
+              location: PlaceLocation(latitude: 2.11, longitude: 5.11)))
+          .toList();
+      notifyListeners();
+    }
+  }
+
--- lib/screens/place_list_screen.dart
+++ lib/screens/place_list_screen.dart
@@ -20,23 +20,35 @@ class PlacesListScreen extends StatelessWidget {
               icon: Icon(Icons.add)),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-          child: const Center(
-            child: Text('Got no places yet, start adding!'),
-          ),
-          builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-              ? ch!
-              : ListView.builder(
-                  itemCount: greatPlaces.items.length,
-                  itemBuilder: (ctx, i) => ListTile(
-                    leading: CircleAvatar(
-                        backgroundImage: FileImage(greatPlaces.items[i].image)),
-                        title: Text(greatPlaces.items[i].title,),
-                        onTap: (){
-                        // Go to Page
-                        },
-                  ),
-                )),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: const Center(
+                  child: Text('Got no places yet, start adding!'),
+                ),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch!
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                              backgroundImage:
+                                  FileImage(greatPlaces.items[i].image)),
+                          title: Text(
+                            greatPlaces.items[i].title,
+                          ),
+                          onTap: () {
+                            // Go to Page
+                          },
+                        ),
+                      )),
+      ),
     );
   }
 }
